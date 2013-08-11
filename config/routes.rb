5e02cb8e5addfa852c5d01ed5240629a57883cb7
The Mini John <admin@phoenix-prime.gr>
2013-08-11 23:40:06 +0300

    Add User Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Flat::Application.routes.draw do
 
+  get "user/show"
   resources :listings
 
   devise_for :users
+	get 'users/:id' => 'user#show', as: :user
+
   get "flatuipro_demo/index"
 	root :to => "listings#index"
 
