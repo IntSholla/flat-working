3edfb1bbb16c4d049bbba471b75e1adc87c42788
The Mini John <admin@phoenix-prime.gr>
2013-08-11 18:52:15 +0300

    Add Listings Scaffold

---
+++ app/models/listing.rb
@@ -0,0 +1,11 @@
+class Listing < ActiveRecord::Base
+
+	# A Listing belogs to ONE User
+	belongs_to :user
+
+	# Validations
+	validates :description, :presence => true
+
+	validates :title, :presence => true, length: { minimum: 15, maximum: 40 }
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Flat::Application.routes.draw do
 
+  resources :listings
+
   devise_for :users
   get "flatuipro_demo/index"
 	root :to => "pages#home"
