bbd53330f7d54b0bd5a678502584eab2530f0e10
The Mini John <admin@phoenix-prime.gr>
2013-08-12 16:01:04 +0300

    Add Price to Listings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130811170636) do
+ActiveRecord::Schema.define(version: 20130812124138) do
 
   create_table "listings", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20130811170636) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.decimal  "price",              precision: 8, scale: 2
   end
 
   add_index "listings", ["user_id"], name: "index_listings_on_user_id"
